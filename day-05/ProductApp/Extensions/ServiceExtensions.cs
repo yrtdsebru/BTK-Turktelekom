f56edc32b0fcfb006cb742a2103cd453dcfd4e39
Ebru <ebru.yrtds@gmail.com>
2022-12-03 21:35:28 +0300

    Mikro project(Areas),Extensions and TempData['info'] has been created.

---
+++ day-05/ProductApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.EFCore;
+
+namespace ProductApp.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, //ben IServiceCol.'u genişletecegim adı da services olucak, genişlettigimiz type'ı veriyoruz.
+            IConfiguration configuration) 
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlconnection"),
+                prj => prj.MigrationsAssembly("ProductApp")));
+
+        }
+    }
+}
